4e207966ca781eeddf87ee93d4f592a4274df780
Mike Korshunov <m.korshunov@me.com>
2020-03-05 23:45:23 -0800

    Adding vagrantfile draft for virtual tests & rm ztp_zscript

---
+++ vagrant/scripts/docker_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install -y --no-install-recommends \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+       "deb https://apt.dockerproject.org/repo/ \
+       ubuntu-$(lsb_release -cs) \
+       main"
+
+sudo apt-get update
+
+sudo apt-get -y install docker-engine
